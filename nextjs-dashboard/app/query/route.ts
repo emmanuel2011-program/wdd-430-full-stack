534184728cec3cf3cea505be86f5a8c4f112783f
Emmanuel <okoyee61@gmail.com>
2025-11-20 09:19:45 +0100

    added item

---
+++ nextjs-dashboard/app/query/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+async function listInvoices() {
+	const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
+
+	return data;
+}
+
+export async function GET() {
+  
+  try {
+    const invoices = await listInvoices();
+    return NextResponse.json(invoices);
+  } catch (error: any) {
+    console.error("QUERY ERROR:", error);
+    return NextResponse.json(
+      { error: error.message || String(error) },
+      { status: 500 }
+    );
+  }
+}
